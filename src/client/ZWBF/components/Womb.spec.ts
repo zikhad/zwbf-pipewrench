d705649ec1830b46fa37d18652a011ea22debefb
Zikhad <zikhad@protonmail.com>
2025-06-18 17:52:14 -0300

    test(mechanics): adding more tests for womb

--- src/client/ZWBF/components/Womb.spec.ts
+++ src/client/ZWBF/components/Womb.spec.ts
@@ -4,11 +4,21 @@ import * as SpyModData from "./ModData";
 import { IsoPlayer } from "@asledgehammer/pipewrench";
 import { WombData } from "../../../types";
 import { Womb } from "./Womb";
+import * as SpyUtils from "../Utils";
 
 jest.mock("@asledgehammer/pipewrench-events");
 jest.mock("./ModData");
+jest.mock("@utils", () => ({
+	...jest.requireActual("@utils"),
+	Inventory: {
+		hasItem: jest.fn()
+	}
+}));
 
 describe("Womb", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
 	describe("without plauer or data", () => {
 		it("Should instantiate with default values", () => {
 			const womb = new Womb();
@@ -18,21 +28,29 @@ describe("Womb", () => {
 			expect(womb.fertility).toBe(0);
 			expect(womb.onContraceptive).toBe(false);
 			expect(womb.phase).toBe("Recovery");
+			expect(womb.image).toBe("");
 		});
 	});
 	describe("With player & data are defined", () => {
 		const mockedPlayer = () => mock<IsoPlayer>();
-		const mockedModData: WombData = {
+		const mockedAnimationUpdateEvent = jest.fn();
+		const mockedModData =  (overrides: Partial<WombData> = {}): WombData => ({
 			amount: 200,
 			total: 400,
 			cycleDay: 1,
 			fertility: 0,
 			onContraceptive: true,
-			chances: Womb.chances
-		};
+			chances: Womb.chances,
+			...overrides
+		});
 		beforeEach(() => {
-			jest.spyOn(SpyModData.ModData.prototype, "data", "get").mockReturnValue(mockedModData)
+			jest.spyOn(SpyModData.ModData.prototype, "data", "get").mockReturnValue(mockedModData())
 			jest.spyOn(SpyEvents.onCreatePlayer, 'addListener').mockImplementation(cb => cb(0, mockedPlayer()));
+			jest.spyOn(SpyEvents.EventEmitter.prototype, 'addListener')
+				.mockImplementation(cb => {
+					mockedAnimationUpdateEvent();
+					cb({ isActive: true, delta: 500, duration: 1000 });
+				});
 		});
 		it("should instantiate with data", () => {
 			const womb = new Womb();
@@ -42,6 +60,7 @@ describe("Womb", () => {
 			expect(womb.fertility).toBe(0);
 			expect(womb.onContraceptive).toBe(true);
 			expect(womb.phase).toBe("Menstruation");
+			expect(mockedAnimationUpdateEvent).toHaveBeenCalled();
 		});
 		it.each(
 			[
@@ -53,29 +72,83 @@ describe("Womb", () => {
 			]
 		)("The cycle should be $phase when cycleday is $day", ({day, phase}) => {
 			jest.spyOn(SpyModData.ModData.prototype, "data", "get")
-				.mockReturnValue({
-					...mockedModData,
-					cycleDay: day
-				});
+				.mockReturnValue(mockedModData({ cycleDay: day }));
 			const womb = new Womb();
 			expect(womb.phase).toBe(phase);
 		});
 
+		describe("get still Images", () => {
+			it.each([
+				{ amount: 0, expected: "media/ui/womb/normal/womb_normal_0.png" },
+				{ amount: 1, expected: "media/ui/womb/normal/womb_normal_1.png" },
+				{ amount: 1000, expected: "media/ui/womb/normal/womb_normal_17.png" },
+			])("Image should be $expected if sperm amount is $amount", ({amount, expected}) => {
+				jest.spyOn(SpyModData.ModData.prototype, "data", "get")
+					.mockReturnValue(mockedModData({ amount }));
+				const womb = new Womb();
+				expect(womb.image).toBe(expected);
+			});
+		});
+
+		describe("get scene Images", () => {
+			it.each([
+				/* Not pregnant */
+				{ amount: 0, isPregnant: false, condom: false, expected: "media/ui/animation/normal/empty/0.png" },
+				{ amount: 0, isPregnant: false, condom: true, expected: "media/ui/animation/condom/0.png" },
+				{ amount: 900, isPregnant: false, condom: false, expected: "media/ui/animation/normal/full/0.png" },
+				{ amount: 900, isPregnant: false, condom: true, expected: "media/ui/animation/condom/0.png" },
+				/* Pregnant */
+				{ amount: 0, isPregnant: true, condom: false, expected: "media/ui/animation/pregnant/0.png" },
+				{ amount: 0, isPregnant: true, condom: true, expected: "media/ui/animation/condom/0.png" },
+				{ amount: 900, isPregnant: true, condom: false, expected: "media/ui/animation/pregnant/0.png" },
+				{ amount: 900, isPregnant: true, condom: true, expected: "media/ui/animation/condom/0.png" },
+			])("Should return $expected when pregnancy: $isPregnant, condom: $condom and amount: $amount", ({isPregnant, condom, amount, expected}) => {
+				jest.spyOn(SpyModData.ModData.prototype, "data", "get")
+					.mockReturnValue(mockedModData({ amount }));
+				jest.spyOn(SpyUtils.Inventory, 'hasItem')
+					.mockReturnValue(condom);
+				
+				const womb = new Womb();
+				womb.onPregnancyUpdate({ isPregnant, progress: 0.6 });
+				womb.onAnimationUpdate({ isActive: true, delta: 500, duration: 1000 });
+				expect(womb.image).toBe(expected);
+			});
+			it("should return labor animation", () => {
+				jest.spyOn(SpyUtils.Inventory, 'hasItem').mockReturnValue(false);
+				const womb = new Womb();
+				womb.onPregnancyUpdate({ isPregnant: true, progress: 1, isInLabor: true });
+				womb.onAnimationUpdate({ isActive: true, delta: 500, duration: 1000 });
+				expect(womb.image).toBe("media/ui/animation/birth/0.png");
+			});
+		});
+
 		describe("Pregnancy", () => {
 			it.each([
-				{ isPregnant: false, progress: 0, phase: "Recovery" },
+				{ isPregnant: false, progress: 0, phase: "Menstruation" },
 				{ isPregnant: true, progress: 0.5, phase: "Pregnant" },
 			])("Cycle phase should be $phase if pregnancy is $pregnancy", ({ isPregnant, progress,  phase }) => {
 				const womb = new Womb();
 				womb.onPregnancyUpdate({ isPregnant, progress });
 				expect(womb.phase).toBe(phase);
 			});
-			it("amount should be 0 is pregnancy progress is > 0.5", () => {
+			it("amount should be 0 if pregnancy progress is > 0.5", () => {
 				const womb = new Womb();
 				expect(womb.amount).toBe(200);
 				womb.onPregnancyUpdate({ isPregnant: true, progress: 0.6 });
 				expect(womb.amount).toBe(0);
-			})
+			});
+			it.each([
+				{ progress: 0, amount: 0, expected: "media/ui/womb/conception/womb_conception_0.png" },
+				{ progress: 0, amount: 1000, expected: "media/ui/womb/conception/womb_conception_17.png" },
+				{ progress: 0.8, amount: 1000, expected: "media/ui/womb/pregnant/womb_pregnant_4.png" },
+				{ progress: 0.95, amount: 1000, expected: "media/ui/womb/pregnant/womb_pregnant_6.png" },
+			])("still image should be $expected when progress is $progress and amount is $amount", ({progress, amount, expected}) => {
+				jest.spyOn(SpyModData.ModData.prototype, "data", "get")
+				.mockReturnValue(mockedModData({ amount }));
+				const womb = new Womb();
+				womb.onPregnancyUpdate({ isPregnant: true, progress });
+				expect(womb.image).toBe(expected);
+			});
 		});
 	});
 });
