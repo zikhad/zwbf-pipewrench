b48f6d5059c9a4feed6253b20ec652953c45dc57
Zikhad <zikhad@protonmail.com>
2025-06-23 21:17:05 -0300

    feat(mechanics): first pregnancy implementation [wip]

--- src/client/ZWBF/components/Pregnancy.ts
+++ src/client/ZWBF/components/Pregnancy.ts
@@ -1,23 +1,31 @@
-/* import { PregnancyData } from "@types";
+import { PregnancyData } from "@types";
 import { Player } from "./Player";
 import { IsoPlayer, triggerEvent } from "@asledgehammer/pipewrench";
-import { ZWBFEvents } from "@constants"; */
-
-class Pregnancy /* extends Player<PregnancyData> */ {
-	/* constructor() {
-        super("ZWBFPregnancy");
+import * as Events from "@asledgehammer/pipewrench-events";
+import { ZWBFEvents, ZWBFTraitsEnum } from "@constants";
+class Pregnancy extends Player<PregnancyData> {
+	constructor() {
+        super();
     }
     protected onCreatePlayer(player: IsoPlayer): void {
-        this.defaultData = {
+        /* this.defaultData = {
             isPregnant: false,
             progress: 0,
             isInLabor: false
-        };
+        };*/
         super.onCreatePlayer(player);
+        new Events.EventEmitter(ZWBFEvents.PREGNANCY_START).addListener(() => this.start());
+    }
+    private start() {
+        this.player?.getTraits().add(ZWBFTraitsEnum.PREGNANCY);
+        this.pregnancy = {
+            progress: 0,
+            isInLabor: false
+        };
     }
     protected onEveryMinute(): void {
-        super.onEveryMinute();
+        if(!this.pregnancy) return;
         //this. = this.data;
-        triggerEvent(ZWBFEvents.PREGNANCY_UPDATE, this.pregnancyData);
-    } */
+        // triggerEvent(ZWBFEvents.PREGNANCY_UPDATE, this.pregnancyData);
+    }
 }
