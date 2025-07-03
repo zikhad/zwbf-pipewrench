7e81d7829264f0d2636c07537a5383dbddb93153
Zikhad <zikhad@protonmail.com>
2025-07-03 00:38:47 -0300

    test(ui): adding tests to UI

--- src/client/ZWBF/components/ZWBFUI.spec.ts
+++ src/client/ZWBF/components/ZWBFUI.spec.ts
@@ -9,10 +9,44 @@ jest.mock("@client/components/UI/ZWBFTabManager", () => ({
 }));
 
 describe("ZWBFUI", () => {
+    const addListener = jest.fn();
+
     const ui = new ZWBFUI({
         lactation: mock(),
         pregnancy:mock(),
         womb: mock()
+    });
+    beforeEach(() => {
+        addListener.mockClear();
+        (Events as any).onCreateUI = { addListener };
+        (Events as any).onPostRender = { addListener };
+        (Events as any).onCreatePlayer = { addListener };
+    });
+    describe("Event System", () => {
+        it.each(
+            [
+                { event: "onCreateUI", handler: "onCreateUI" },
+                { event: "onPostRender", handler: "onUpdateUI" },
+                { event: "onCreatePlayer", handler: "onCreatePlayer" }
+            ]
+        )("Should register & call $event callback properly", ({event, handler}) => {
+            (Events as any)[event] = { addListener };
+
+            const ui = new ZWBFUI({
+                lactation: mock(),
+                pregnancy: mock(),
+                womb: mock()
+            });
+
+            (ui as any)[handler] = jest.fn();
+            const spy = jest.spyOn(ui as any, handler);
+
+            expect(addListener).toHaveBeenCalled();
+            const [callback] = addListener.mock.calls[0];
+            
+            callback();
+            expect(spy).toHaveBeenCalled();
+        })
     });
     it("should work", () => {
         expect(ui).toBeDefined();
